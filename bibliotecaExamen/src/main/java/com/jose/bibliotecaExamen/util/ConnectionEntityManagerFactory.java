197301fc4bd0329b92fc4c8d3ebc9f619d0ecb28
junio <junio@LAPTOP-TI8Q6V9F>
2019-12-10 20:22:43 +0100

    TODO COMENTADO

--- bibliotecaExamen/src/main/java/com/jose/bibliotecaExamen/util/ConnectionEntityManagerFactory.java
+++ bibliotecaExamen/src/main/java/com/jose/bibliotecaExamen/util/ConnectionEntityManagerFactory.java
@@ -4,21 +4,21 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 public class ConnectionEntityManagerFactory {
 
-	private static EntityManagerFactory entityManagerFactory = null;
+	private static EntityManagerFactory entityManagerFactory = null;//instaciamos el obejto de su porpia clase a null
 	
-	private ConnectionEntityManagerFactory() {
+	private ConnectionEntityManagerFactory() {//creamos un constructor
 		
-		entityManagerFactory = Persistence.createEntityManagerFactory("com.jose.bibliotecaExamen.H2");
+		entityManagerFactory = Persistence.createEntityManagerFactory("com.jose.bibliotecaExamen.H2");//el constructor iguala el objeto que creamos antes al entity que genera el persistence
 	}
 	
-	
+	/*CREAMOS UN METODO STATICO PARA LLAMAR AL CONSTRUCTOR Y QUE NOS PERMITA CREAR EL OBEJTO SOLO UNA VE Z*/
 	
 	public static EntityManagerFactory getEntityManager() {
 		
-		if(entityManagerFactory==null) {
+		if(entityManagerFactory==null) {//COMPROBAMOS SI EXISTE EL ENTITY
 			
-			new ConnectionEntityManagerFactory();
+			new ConnectionEntityManagerFactory();//SI NO EXISTE LO CREA
 		}
-		return entityManagerFactory;
+		return entityManagerFactory;//SI EXISTE LO DEVUELVO
 	}
 }
